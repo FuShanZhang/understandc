1c2bcf1b79113e24e6cbf2c48a25579a317a8a78
edz <edz@zhangfushan.local>
2019-10-23 17:28:16 +0800

    ignore

---
+++ ch01/example/e02.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(void){
+	int rlong;
+	int rwide;
+	scanf("%s,%d,%d","请输入长宽",&rlong,&rwide);
+	printf("%d\n",rlong*rwide );
+	return 0;
+}
---
+++ ch01/example/e03.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(int argc,char const *argv[]){
+	int inputNum,inputTwitce;
+
+	scanf("%d,%d",&inputNum,&inputTwitce);
+
+	printf("scanf的返回值是%d,%d\n", inputNum,inputTwitce);
+}
