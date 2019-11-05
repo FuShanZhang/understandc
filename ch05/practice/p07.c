5d9538d607638862e466a9c030a9f457ca036ccc
张福山 <zhangfushan@zhangfuhandeMBP.lan>
2019-11-05 22:56:33 +0800

    20191105

--- ch05/practice/p07.c
+++ ch05/practice/p07.c
@@ -1,26 +1,8 @@
+/*
+	利用宏声明数组元素个数。并显示
+*/
 #include<stdio.h>
-#define NUMBER 10
+#define ELEMENT 4;
 int main(void){
-	int num ;
-	printf("数据个数:");	
-
-	do{
-		scanf("%d",&num);
-		if(num<1 || num>NUMBER){
-			printf("请输入1～%d的数:",NUMBER);
-		}
-	}while(num < 1 || num>NUMBER);
-
-	int arr[num];
-
-	for(int i = 1 ; i <= num ; i++){
-		printf("%d号:",i);
-		scanf("%d",&arr[i-1]);
-	}
-	printf("{");
-	for(int i = 0 ; i<num ; i++){
-		printf(" %d ",arr[i] );
-	}
-	printf("}");
-	printf("\n");
+	printf("数据个数：");	scanf("%d",&)
 }
